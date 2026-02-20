2e956ce44093f71094cbb0ea42a2060986c84446
palindungan <rizkika.palindungan1@gmail.com>
2026-02-20 21:23:55 +0700

    layouts\master1

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/master1', function () {
+    return view('components.layouts.master1.master');
+});
