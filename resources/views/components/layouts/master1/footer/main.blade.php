edda26c02bedae51c529b282ae1e3f9484c3d4b8
palindungan <rizkika.palindungan1@gmail.com>
2026-02-20 22:01:09 +0700

    layouts\master1\footer

---
+++ resources/views/components/layouts/master1/footer/main.blade.php
@@ -0,0 +1,15 @@
+<footer class="footer">
+    <div class="container-fluid">
+        <nav class="pull-left">
+            <ul class="nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="https://wa.me/6281249138367" target="_blank">Chat via WhatsApp</a>
+                </li>
+            </ul>
+        </nav>
+        <div class="copyright ms-auto">
+            2026, made with <i class="fa fa-heart heart text-danger"></i>
+            by <a href="https://www.linkedin.com/in/rizkika-zakka-palindungan-ab5a02193">Rizkika</a>
+        </div>
+    </div>
+</footer>
--- resources/views/components/layouts/master1/master.blade.php
+++ resources/views/components/layouts/master1/master.blade.php
@@ -297,35 +297,9 @@
 					@yield('content')
 				</div>
 			</div>
-			
-			<footer class="footer">
-				<div class="container-fluid">
-					<nav class="pull-left">
-						<ul class="nav">
-							<li class="nav-item">
-								<a class="nav-link" href="http://www.themekita.com">
-									ThemeKita
-								</a>
-							</li>
-							<li class="nav-item">
-								<a class="nav-link" href="#">
-									Help
-								</a>
-							</li>
-							<li class="nav-item">
-								<a class="nav-link" href="#">
-									Licenses
-								</a>
-							</li>
-						</ul>
-					</nav>
-					<div class="copyright ms-auto">
-						2024, made with <i class="fa fa-heart heart text-danger"></i> by <a href="http://www.themekita.com">ThemeKita</a>
-					</div>				
-				</div>
-			</footer>
-		</div>
 
+			@include('components.layouts.master1.footer.main')
+		</div>
 	</div>
 
 	@include('components.layouts.master1.script.main')
