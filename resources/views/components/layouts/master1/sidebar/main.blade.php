dd0b9e9255f310c69840770fcd0b4b1ef0ef5352
palindungan <rizkika.palindungan1@gmail.com>
2026-02-20 21:44:15 +0700

    layouts\master1\sidebar

---
+++ resources/views/components/layouts/master1/sidebar/main.blade.php
@@ -0,0 +1,39 @@
+<!-- Sidebar -->
+<div class="sidebar" data-background-color="dark">
+    <div class="sidebar-logo">
+        <!-- Logo Header -->
+        <div class="logo-header" data-background-color="dark">
+
+            <a href="" class="logo">
+                <img src="{{ asset('kaiadmin_lite/examples/demo1/assets/img/kaiadmin/logo_light.svg') }}"
+                    alt="navbar brand" class="navbar-brand" height="20">
+            </a>
+            <div class="nav-toggle">
+                <button class="btn btn-toggle toggle-sidebar">
+                    <i class="gg-menu-right"></i>
+                </button>
+                <button class="btn btn-toggle sidenav-toggler">
+                    <i class="gg-menu-left"></i>
+                </button>
+            </div>
+            <button class="topbar-toggler more">
+                <i class="gg-more-vertical-alt"></i>
+            </button>
+
+        </div>
+        <!-- End Logo Header -->
+    </div>
+    <div class="sidebar-wrapper scrollbar scrollbar-inner">
+        <div class="sidebar-content">
+            <ul class="nav nav-secondary">
+                <li class="nav-item">
+                    <a href="">
+                        <i class="fas fa-desktop"></i>
+                        <p>Dashboard</p>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+<!-- End Sidebar -->
