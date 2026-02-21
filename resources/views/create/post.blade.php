d6e5f2ee4bab34d35d6f4d1ecaf0dd86e192a2f9
palindungan <rizkika.palindungan1@gmail.com>
2026-02-21 14:23:05 +0700

    Update post.blade.php

--- resources/views/create/post.blade.php
+++ resources/views/create/post.blade.php
@@ -56,6 +56,7 @@
         Dropzone.options.myGreatDropzone = { // camelized version of the `id`
             paramName: "file", // The name that will be used to transfer the file
             maxFilesize: 2, // MB
+            addRemoveLinks: true,
             accept: function(file, done) {
                 if (file.name == "justinbieber.jpg") {
                     done("Naha, you don't.");
