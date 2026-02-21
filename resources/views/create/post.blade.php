7bc789ee30684c8fbbb9005606c748ec0098b921
palindungan <rizkika.palindungan1@gmail.com>
2026-02-21 14:16:49 +0700

    routes/api

--- resources/views/create/post.blade.php
+++ resources/views/create/post.blade.php
@@ -17,7 +17,7 @@
 
                         <!-- KIRI: Gambar -->
                         <div class="col-md-7 p-0">
-                            <form action="/target" class="dropzone" id="my-great-dropzone" style="height: 60vh;"></form>
+                            <form action="api/v1/target" class="dropzone" id="my-great-dropzone" style="height: 60vh;"></form>
                         </div>
 
                         <!-- KANAN: Text -->
