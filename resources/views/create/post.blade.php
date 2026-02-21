8fe62d48f73018917667de6f6eeea0254f352c6e
palindungan <rizkika.palindungan1@gmail.com>
2026-02-21 13:42:52 +0700

    Update post.blade.php

--- resources/views/create/post.blade.php
+++ resources/views/create/post.blade.php
@@ -16,44 +16,8 @@
                     <div class="row g-0">
 
                         <!-- KIRI: Gambar -->
-                        <div class="col-md-7 bg-dark p-0" style="max-height: 90vh;">
-                            <div id="postCarousel" class="carousel slide h-100" data-bs-touch="true">
-
-                                <!-- Indicators -->
-                                <div class="carousel-indicators">
-                                    <button type="button" data-bs-target="#postCarousel" data-bs-slide-to="0"
-                                        class="active"></button>
-                                    <button type="button" data-bs-target="#postCarousel" data-bs-slide-to="1"></button>
-                                    <button type="button" data-bs-target="#postCarousel" data-bs-slide-to="2"></button>
-                                </div>
-
-                                <!-- Wrapper gambar -->
-                                <div class="carousel-inner h-100">
-                                    <div class="carousel-item active h-100">
-                                        <img src="{{ asset('kaiadmin_lite/examples/demo1/assets/img/blogpost.jpg') }}"
-                                            class="d-block w-100 h-100 object-fit-cover" alt="Gambar 1">
-                                    </div>
-                                    <div class="carousel-item h-100">
-                                        <img src="{{ asset('kaiadmin_lite/examples/demo1/assets/img/bg-404.jpeg') }}"
-                                            class="d-block w-100 h-100 object-fit-cover" alt="Gambar 2">
-                                    </div>
-                                    <div class="carousel-item h-100">
-                                        <img src="{{ asset('kaiadmin_lite/examples/demo1/assets/img/jm_denis.jpg') }}"
-                                            class="d-block w-100 h-100 object-fit-cover" alt="Gambar 3">
-                                    </div>
-                                </div>
-
-                                <!-- Controls -->
-                                <button class="carousel-control-prev" type="button" data-bs-target="#postCarousel"
-                                    data-bs-slide="prev">
-                                    <span class="carousel-control-prev-icon"></span>
-                                </button>
-                                <button class="carousel-control-next" type="button" data-bs-target="#postCarousel"
-                                    data-bs-slide="next">
-                                    <span class="carousel-control-next-icon"></span>
-                                </button>
-
-                            </div>
+                        <div class="col-md-7 p-0" style="max-height: 90vh;">
+                            <form action="/target" class="dropzone" id="my-great-dropzone"></form>
                         </div>
 
                         <!-- KANAN: Text -->
@@ -70,7 +34,8 @@
 
                             <!-- Textarea -->
                             <div class="flex-grow-1 d-flex p-3">
-                                <textarea rows="15" class="form-control border-0 shadow-none h-100" placeholder="Write a caption..." style="resize:none"></textarea>
+                                <textarea rows="15" class="form-control border-0 shadow-none h-100" placeholder="Write a caption..."
+                                    style="resize:none"></textarea>
                             </div>
 
                         </div>
@@ -83,3 +48,19 @@
         </div>
     </div>
 </div>
+
+@push('scripts')
+    <script>
+        Dropzone.options.myGreatDropzone = { // camelized version of the `id`
+            paramName: "file", // The name that will be used to transfer the file
+            maxFilesize: 2, // MB
+            accept: function(file, done) {
+                if (file.name == "justinbieber.jpg") {
+                    done("Naha, you don't.");
+                } else {
+                    done();
+                }
+            }
+        };
+    </script>
+@endpush
