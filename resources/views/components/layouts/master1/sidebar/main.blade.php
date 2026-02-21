ca29ae85a7475ad5440422222acc702878bc47bc
palindungan <rizkika.palindungan1@gmail.com>
2026-02-22 03:05:42 +0700

    Update main.blade.php

--- resources/views/components/layouts/master1/sidebar/main.blade.php
+++ resources/views/components/layouts/master1/sidebar/main.blade.php
@@ -4,10 +4,10 @@
         <!-- Logo Header -->
         <div class="logo-header" data-background-color="dark">
 
-            <a href="" class="logo">
+            {{-- <a href="" class="logo">
                 <img src="{{ asset('kaiadmin_lite/examples/demo1/assets/img/kaiadmin/logo_light.svg') }}"
                     alt="navbar brand" class="navbar-brand" height="20">
-            </a>
+            </a> --}}
             <div class="nav-toggle">
                 <button class="btn btn-toggle toggle-sidebar">
                     <i class="gg-menu-right"></i>
