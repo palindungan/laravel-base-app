a92b33ef499750de8c375698fdb38f3ee9691548
palindungan <rizkika.palindungan1@gmail.com>
2026-02-21 12:47:52 +0700

    Update show.blade.php

--- resources/views/explore/show.blade.php
+++ resources/views/explore/show.blade.php
@@ -11,11 +11,48 @@
                 <div class="container-fluid">
                     <div class="row g-0">
 
-                        <!-- KIRI: Gambar -->
+                        <!-- KIRI: Carousel Gambar (Bootstrap default) -->
                         <div class="col-md-7 bg-dark p-0" style="max-height: 90vh;">
-                            <div class="img-wrapper h-100 d-flex justify-content-center align-items-center">
-                                <img src="{{ asset('kaiadmin_lite/examples/demo1/assets/img/blogpost.jpg') }}"
-                                    class="img-fluid object-fit-cover" alt="Konten Gambar" />
+                            <div id="postCarousel" class="carousel slide h-100" data-bs-touch="true">
+
+                                <!-- Indicators -->
+                                <div class="carousel-indicators">
+                                    <button type="button" data-bs-target="#postCarousel" data-bs-slide-to="0"
+                                        class="active"></button>
+                                    <button type="button" data-bs-target="#postCarousel" data-bs-slide-to="1"></button>
+                                    <button type="button" data-bs-target="#postCarousel" data-bs-slide-to="2"></button>
+                                </div>
+
+                                <!-- Wrapper gambar -->
+                                <div class="carousel-inner h-100">
+
+                                    <div class="carousel-item active h-100">
+                                        <img src="{{ asset('kaiadmin_lite/examples/demo1/assets/img/blogpost.jpg') }}"
+                                            class="d-block w-100 h-100 object-fit-cover" alt="Gambar 1">
+                                    </div>
+
+                                    <div class="carousel-item h-100">
+                                        <img src="{{ asset('kaiadmin_lite/examples/demo1/assets/img/bg-404.jpeg') }}"
+                                            class="d-block w-100 h-100 object-fit-cover" alt="Gambar 2">
+                                    </div>
+
+                                    <div class="carousel-item h-100">
+                                        <img src="{{ asset('kaiadmin_lite/examples/demo1/assets/img/jm_denis.jpg') }}"
+                                            class="d-block w-100 h-100 object-fit-cover" alt="Gambar 3">
+                                    </div>
+
+                                </div>
+
+                                <!-- Controls -->
+                                <button class="carousel-control-prev" type="button" data-bs-target="#postCarousel"
+                                    data-bs-slide="prev">
+                                    <span class="carousel-control-prev-icon"></span>
+                                </button>
+                                <button class="carousel-control-next" type="button" data-bs-target="#postCarousel"
+                                    data-bs-slide="next">
+                                    <span class="carousel-control-next-icon"></span>
+                                </button>
+
                             </div>
                         </div>
 
