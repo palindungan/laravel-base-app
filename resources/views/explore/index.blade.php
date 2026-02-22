621ca5243a097bbb9cf95b8dedc9c273db956e02
palindungan <rizkika.palindungan1@gmail.com>
2026-02-22 09:37:34 +0700

    Update index.blade.php

--- resources/views/explore/index.blade.php
+++ resources/views/explore/index.blade.php
@@ -4,8 +4,8 @@
 
 @section('content')
     <div class="page-inner">
-        <div class="row">
-            <div class="col-md-4">
+        <div class="row" id="post-list">
+            {{-- <div class="col-md-4">
                 <button type="button" class="btn p-0 border-0 bg-transparent" data-bs-toggle="modal"
                     data-bs-target="#instagramModal">
                     <div class="card card-profile card-post card-round insta-hover">
@@ -22,9 +22,9 @@
                         </div>
                     </div>
                 </button>
+            </div> --}}
 
-                {{-- posts --}}
-            </div>
+            {{-- posts --}}
         </div>
     </div>
 
@@ -53,6 +53,34 @@
                     console.log('================/api/v1/posts================');
                     console.log(res);
                     console.log('=============================================');
+
+                    let posts = res.data;
+                    let html = '';
+
+                    posts.forEach(function(post) {
+                        html += `
+                            <div class="col-md-4">
+                                <button type="button" class="btn p-0 border-0 bg-transparent" data-bs-toggle="modal"
+                                    data-bs-target="#instagramModal">
+                                    <div class="card card-profile card-post card-round insta-hover">
+                                        <div class="img-wrapper">
+                                            <img src="{{ asset('kaiadmin_lite/examples/demo1/assets/img/blogpost.jpg') }}"
+                                                alt="Card image cap">
+
+                                            <div class="hover-info">
+                                                <div class="hover-stats">
+                                                    <span>❤️ 4000</span>
+                                                    <span>💬 2000</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </button>
+                            </div>
+                        `;
+                    });
+
+                    $('#post-list').html(html);
                 },
                 error: function(xhr) {
                     console.log(xhr.status, xhr.responseText);
