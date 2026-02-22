6ff7f4b15c596d53afe5ceede8c7311b9ce5986d
palindungan <rizkika.palindungan1@gmail.com>
2026-02-22 11:49:31 +0700

    Update PostLikeController.php

--- app/Http/Controllers/API/V1/PostLikeController.php
+++ app/Http/Controllers/API/V1/PostLikeController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Models\PostLike;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PostLikeController extends Controller
 {
@@ -20,7 +22,35 @@ class PostLikeController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // validate the request data post_id, status
+        $validated = $request->validate([
+            'post_id' => 'required|exists:posts,id',
+            'status' => 'required|in:like,unlike'
+        ]);
+
+        $data = [
+            'post_id' => $validated['post_id'],
+            'user_id' => Auth::user()->id,
+        ];
+
+        // check status and create or delete the like
+        if ($validated['status'] === 'like') {
+            // create a new like for the post
+            $like = PostLike::updateOrCreate(
+                $data,
+                $data
+            );;
+            return response()->json($like, 201);
+        } else {
+            // delete the like for the post
+            $like = PostLike::where('post_id', $validated['post_id'])
+                ->where('user_id', Auth::user()->id)
+                ->first();
+            if ($like) {
+                $like->delete();
+            }
+            return response()->json(null, 204);
+        }
     }
 
     /**
