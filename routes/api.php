d02a357cccb62cdb454ecf551092261443c7412d
palindungan <rizkika.palindungan1@gmail.com>
2026-02-22 10:44:48 +0700

    apiResource('post_comments'

--- routes/api.php
+++ routes/api.php
@@ -5,4 +5,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->prefix('v1')->group(function () {
     Route::apiResource('posts', \App\Http\Controllers\Api\V1\PostController::class);
+    Route::apiResource('post_comments', \App\Http\Controllers\Api\V1\PostCommentController::class);
 });
