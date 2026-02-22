15c87cbc5be703f5bd383af93588e7d0f529bedd
palindungan <rizkika.palindungan1@gmail.com>
2026-02-22 09:49:37 +0700

    Update index.blade.php

--- resources/views/explore/index.blade.php
+++ resources/views/explore/index.blade.php
@@ -58,13 +58,20 @@
                     let html = '';
 
                     posts.forEach(function(post) {
+                        post_files = post.post_files;
+
+                        file_path_storage = "{{ asset('kaiadmin_lite/examples/demo1/assets/img/blogpost.jpg') }}";
+                        if (post_files.length > 0) {
+                            file_path_storage = post_files[0].file_path_storage;
+                        }
+
                         html += `
                             <div class="col-md-4" id="post-${post.id}">
                                 <button type="button" class="btn p-0 border-0 bg-transparent" data-bs-toggle="modal"
                                     data-bs-target="#instagramModal">
                                     <div class="card card-profile card-post card-round insta-hover">
                                         <div class="img-wrapper">
-                                            <img src="{{ asset('kaiadmin_lite/examples/demo1/assets/img/blogpost.jpg') }}"
+                                            <img src="${file_path_storage}"
                                                 alt="Card image cap">
 
                                             <div class="hover-info">
