a157d30b5f9f486804fc3e6ab4fcb247977ac553
palindungan <rizkika.palindungan1@gmail.com>
2026-02-22 09:01:28 +0700

    Update index.blade.php

--- resources/views/explore/index.blade.php
+++ resources/views/explore/index.blade.php
@@ -5,26 +5,24 @@
 @section('content')
     <div class="page-inner">
         <div class="row">
-            @for ($i = 0; $i < 7; $i++)
-                <div class="col-md-4">
-                    <button type="button" class="btn p-0 border-0 bg-transparent" data-bs-toggle="modal"
-                        data-bs-target="#instagramModal">
-                        <div class="card card-profile card-post card-round insta-hover">
-                            <div class="img-wrapper">
-                                <img src="{{ asset('kaiadmin_lite/examples/demo1/assets/img/blogpost.jpg') }}"
-                                    alt="Card image cap">
+            <div class="col-md-4">
+                <button type="button" class="btn p-0 border-0 bg-transparent" data-bs-toggle="modal"
+                    data-bs-target="#instagramModal">
+                    <div class="card card-profile card-post card-round insta-hover">
+                        <div class="img-wrapper">
+                            <img src="{{ asset('kaiadmin_lite/examples/demo1/assets/img/blogpost.jpg') }}"
+                                alt="Card image cap">
 
-                                <div class="hover-info">
-                                    <div class="hover-stats">
-                                        <span>❤️ 4000</span>
-                                        <span>💬 2000</span>
-                                    </div>
+                            <div class="hover-info">
+                                <div class="hover-stats">
+                                    <span>❤️ 4000</span>
+                                    <span>💬 2000</span>
                                 </div>
                             </div>
                         </div>
-                    </button>
-                </div>
-            @endfor
+                    </div>
+                </button>
+            </div>
         </div>
     </div>
 
